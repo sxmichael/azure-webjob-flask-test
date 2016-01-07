e3fc9c4f7d70f5986fe322406288ea83a07b2a45
Michael Gelfand <michael.gelfand@gartner.com>
2016-01-07 16:40:20 +0000

    case

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -3,8 +3,34 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request
 from FlaskWebProject import app
+import time
+import random
+import json
+
+class Stats(object):
+    def __init__(self):
+        self.receive_time = str(datetime.now())
+        self.total_start_time = time.time()
+        self.total_time = None
+        self.send_time = None
+        self.processed = 0
+
+    def end(self):
+        self.total_time = time.time() - self.total_start_time
+        self.send_time = str(datetime.now())
+
+    def dumps(self):
+        return {
+            "receive_time" : self.receive_time,
+            "send_time" : self.send_time,
+            "total_time" : self.total_time,
+            "processed": self.processed,
+        }
+
+def log_info(message, unique_id):
+    app.logger.info(message, extra={'guid': unique_id})
 
 @app.route('/')
 @app.route('/home')
@@ -35,3 +61,22 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+@app.route('/do', methods = ['POST'])
+def post_something():
+    current_id = request.args.get('id', 'none')
+    stats = Stats()
+    names = request.data.split('|')
+
+    log_info("start working on: " + current_id, current_id)
+    
+    should_take = random.randrange(100, 150)
+    for name in names:
+        time.sleep(should_take / 5000.0)
+        stats.processed += 1
+
+    stats.end()
+    stat_string = json.dumps(stats.dumps(), indent=4)
+    log_info(stat_string, current_id)
+    log_info("finish working on: {current_id}".format(current_id=current_id), current_id)
+    return stat_string
